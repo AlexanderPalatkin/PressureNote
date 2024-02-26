02bf69c8d13eabd866bcf8fa60b367b489993d20
AlexanderPalatkin <palatkin.alexander@gmail.com>
2024-02-27 00:37:12 +0300

    Добавил возможность удаления показаний о давлении. Рефакторинг кода

--- app/src/main/java/com/example/pressurenote/ui/healthlist/HealthListFragment.kt
+++ app/src/main/java/com/example/pressurenote/ui/healthlist/HealthListFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.pressurenote.databinding.AlertDialogAddIndicationBinding
